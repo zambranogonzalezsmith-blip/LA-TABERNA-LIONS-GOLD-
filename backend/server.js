dfae7c1258a97308ee8fb0f5ed7b28fdf17e209a
Carlos Smith Zambrano <zambranogonzalezsmith@gmail.com>
2026-04-02 19:15:16 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const dotenv = require('dotenv');
 
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+const db = new sqlite3.Database('./database/albion_p2p.db');
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+    res.send('Servidor P2P LIONS-GOLD Operativo');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
